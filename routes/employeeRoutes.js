9eace5795f3fac33cdc4de5b367f611178190535
Umar Ejaz Imam <123633963+umarei@users.noreply.github.com>
2024-09-20 19:20:23 +0530

    employeeRoutes.js

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const { createEmployee, getEmployees } = require('../controllers/employeeController');
 const router = express.Router();
 
+// Route to create a new employee
 router.post('/create', createEmployee);
+
+// Route to get all employees
 router.get('/', getEmployees);
 
+// Export the router
 module.exports = router;
